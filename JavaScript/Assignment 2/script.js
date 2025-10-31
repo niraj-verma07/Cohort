a88cdceb7c39a0f13493dd85e95ac11a40d476d9
niraj-verma07 <vermaniraj9607@gmail.com>
2025-10-31 22:31:13 +0530

    Completed

--- JavaScript/Assignment 2/script.js
+++ JavaScript/Assignment 2/script.js
@@ -89,3 +89,116 @@ test(); //print hello
 function test() {
   console.log("Hello");
 }
+
+// d. Try writing a function expression before initialization and call it:
+// hello();
+var hello = function () {
+  console.log("Hi");
+};
+// Write what happened and why.
+
+// 3. Conditional Operators (if, else, else-if, ternary, switch)
+
+// a. Take input using prompt for age.
+// If age > 18 → log “Adult”.
+// Else → log “Minor”.
+
+let age = 15;
+if (age > 18) {
+  console.log("Adult");
+} else {
+  console.log("Minor");
+}
+
+// b. Write a program:
+// If marks >= 90 → “A grade”
+// Else if marks >= 75 → “B grade”
+// Else if marks >= 50 → “C grade”
+// Else → “Fail”
+
+let marks = 95;
+
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 75) {
+  console.log("Grade B");
+} else if (marks >= 50) {
+  console.log("Grade C");
+} else {
+  console.log("Fail");
+}
+
+// c. Create a variable city = “Bhopal”.
+// If city is “Bhopal” → log “MP”
+// Else if city is “Delhi” → log “Capital”
+// Else → log “Unknown City”
+
+let city = "Bhopal";
+if (city === "Bhopal") {
+  console.log("MP");
+} else if (city === "Delhi") {
+  console.log("Delhi");
+} else {
+  console.log("Unknown City");
+}
+
+// d. Use ternary operator:
+// Let score = 40.
+// If score > 35 → “Pass” else “Fail” using a ternary.
+
+let score = 0;
+score > 35 ? console.log("Pass") : console.log("Fail");
+
+// e. Convert this if-else into a ternary:
+// if (temperature > 30) { “Hot” } else { “Pleasant” }
+
+let temperature = 50;
+
+temperature > 30 ? console.log("HOT") : console.log("Pleasent");
+
+// f. Write a switch case:
+// Take day number (1 to 7).
+// Print the day name.
+// Default case: “Invalid Day”.
+
+let Day = 4;
+
+switch (Day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thrusday");
+    break;
+
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid Day");
+}
+
+// g. Using logical operators in condition:
+// If age > 18 and country == “India” → log “Eligible for Vote”
+// Else → “Not Eligible”
+
+let ages = 20;
+let country = "India";
+
+if (ages > 18 && country === "India") {
+  console.log("Eligible for vote");
+} else {
+  console.log("Not Eligible");
+}
