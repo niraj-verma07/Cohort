f3465e128f9f38ebaf91fe044e880ac439a19a71
Niraj Verma <vermaniraj9607@gmail.com>
2025-11-12 20:56:59 +0530

    Level  2 50% done

--- JavaScript/Assignment 4/script.js
+++ JavaScript/Assignment 4/script.js
@@ -1,3 +1,5 @@
+// LEVEL 1
+
 // 1. Write a function sayHello() that prints "Hello JavaScript".
 function sayHello() {
   console.log("Hello");
@@ -38,8 +40,7 @@ function addUnlimited(...nums) {
 
   console.log(ans);
 }
-
-addUnlimited(20, 10, 60, 50, 60);
++addUnlimited(20, 10, 60, 50, 60);
 
 // 5. Create an IIFE that prints "I run instantly!".
 (function () {
@@ -97,3 +98,58 @@ for (const ele in obj) {
 setTimeout(() => {
   console.log("Time's up!");
 }, 2000);
+
+//LEVEL 2
+
+// 1. Write a higher-order function runTwice(fn) that takes another function and executes it two times.
+
+function runTwice(fn) {
+  fn();
+  fn();
+}
+
+runTwice(function () {
+  console.log("Hello");
+});
+
+// 2. Create one pure function that always returns the same output for a given input, and one impure function using a global variable.
+
+function pure(a, b) {
+  console.log(a + b);
+}
+
+let global = 0;
+
+function impure(a) {
+  global++;
+  console.log(a + global);
+}
+
+pure(1, 2);
+pure(1, 2);
+
+impure(2);
+impure(2);
+
+// 3. Write a function that uses object destructuring inside parameters to extract and print name and age.
+
+function object({ name, age }) {
+  console.log(name);
+  console.log(age);
+}
+
+object({ name: "Niraj", age: 20 });
+
+// 4. Demonstrate the difference between normal function and arrow function when used as object methods (the this issue).
+
+// 5. Given an array of numbers, use map() to create a new array where each number is squared.
+
+// 6. Use filter() to get only even numbers from an array.
+
+// 7. Use reduce() to find the total salary from an array of numbers [1000, 2000, 3000].
+
+// 8. Create an array of names and use some() and every() to test a condition (e.g., all names longer than 3 chars).
+
+// 9. Create an object user and test the behavior of Object.freeze() and Object.seal() by adding/changing keys.
+
+// 10. Create a nested object (user → address → city) and access the city name inside it.
