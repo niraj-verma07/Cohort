350c7a36fc64ae908ebff6c919fc088475c7498d
Niraj Verma <vermaniraj9607@gmail.com>
2025-11-20 11:15:03 +0530

    Improved Version

--- JavaScript/DOM Assignment 5/script.js
+++ JavaScript/DOM Assignment 5/script.js
@@ -2,43 +2,51 @@ let img = document.querySelector("img");
 let love = document.querySelector("#love");
 let likeBtn = document.querySelector("#likebtn");
 
-img.addEventListener("dblclick", function () {
+let isLiked = false;
+
+// Function to play heart animation
+function playHeartAnimation() {
   love.style.opacity = 1;
   love.style.transform = "translate(-50%,-50%) scale(1) rotate(0deg)";
 
-  setTimeout(function () {
+  setTimeout(() => {
     love.style.transform = "translate(-50%,-300%) scale(1) rotate(60deg)";
-  }, 800);
-  setTimeout(function () {
+  }, 600);
+
+  setTimeout(() => {
     love.style.opacity = 0;
-  }, 1000);
-  setTimeout(function () {
+  }, 800);
+
+  setTimeout(() => {
     love.style.transform = "translate(-50%,-50%) scale(0) rotate(-60deg)";
-  }, 1200);
+  }, 1000);
+}
+
+// Double Click Like Feature
+
+img.addEventListener("dblclick", function () {
+  isLiked = true;
+
+  likeBtn.classList.remove("ri-heart-line");
+  likeBtn.classList.add("ri-heart-fill");
+  likeBtn.style.color = "red";
+
+  playHeartAnimation();
 });
 
-likeBtn.addEventListener("click", function () {
-  love.style.opacity = 1;
-  love.style.transform = "translate(-50%,-50%) scale(1) rotate(0deg)";
+// Manual Click Like Button
 
-  // toggle icon
-  if (likeBtn.classList.contains("ri-heart-line")) {
+likeBtn.addEventListener("click", function () {
+  isLiked = !isLiked;
+
+  if (isLiked) {
     likeBtn.classList.remove("ri-heart-line");
     likeBtn.classList.add("ri-heart-fill");
     likeBtn.style.color = "red";
+    playHeartAnimation(); // play animation only when liking
   } else {
     likeBtn.classList.remove("ri-heart-fill");
     likeBtn.classList.add("ri-heart-line");
     likeBtn.style.color = "black";
   }
-
-  setTimeout(function () {
-    love.style.transform = "translate(-50%,-300%) scale(1) rotate(60deg)";
-  }, 800);
-  setTimeout(function () {
-    love.style.opacity = 0;
-  }, 1000);
-  setTimeout(function () {
-    love.style.transform = "translate(-50%,-50%) scale(0) rotate(-60deg)";
-  }, 1200);
 });
