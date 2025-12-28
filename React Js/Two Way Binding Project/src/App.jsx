e19beb50ccb0875cbdb07a76e834d31ae7e47aa0
niraj-verma07 <vermaniraj9607@gmail.com>
2025-12-28 22:34:31 +0530

    Project Completed

--- React Js/Two Way Binding Project/src/App.jsx
+++ React Js/Two Way Binding Project/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Form from "./components/Form";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="h-screen w-full bg-gray-800 px-10 py-10 flex flex-col gap-2">
+      <Form />
+    </div>
+  );
 };
 
 export default App;
