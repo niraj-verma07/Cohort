40a4472340cc8b62bf0a1b1864987dc60a002458
niraj-verma07 <vermaniraj9607@gmail.com>
2025-12-28 18:48:20 +0530

    Two Way binding

--- React Js/Two way binding/src/App.jsx
+++ React Js/Two way binding/src/App.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [name, setName] = useState(""); //Input ki value ko manipulate karne ke liye useState ka use karte hai
 
-export default App
+  const [allUsers, setAllUsers] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Submit Ho gaya");
+    const oldUsers = [...allUsers];
+    oldUsers.push(name);
+
+    setAllUsers(oldUsers);
+
+    console.log(oldUsers);
+    setName(""); //Form submit hone par input filed empty ho jayga
+  };
+
+  return (
+    <div className="form">
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          required
+          onChange={(e) => {
+            console.log(e.target.value);
+            setName(e.target.value); //Two Way Binding
+          }}
+        />
+        <button>Submit</button>
+      </form>
+
+      {allUsers.map((elem, idx) => {
+        return <h1 key={idx}>{elem}</h1>;
+      })}
+    </div>
+  );
+};
+
+export default App;
