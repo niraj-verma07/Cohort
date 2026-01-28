97541a16bb0a919b317eef35d50561b55a10ac3b
niraj-verma07 <vermaniraj9607@gmail.com>
2026-01-28 13:02:46 +0530

    Database Connection

--- Backend/Day 6/server.js
+++ Backend/Day 6/server.js
@@ -1,20 +1,18 @@
-// Server start Karna
-// Database se connect Karna
 const app = require("./src/app");
 const mongoose = require("mongoose");
 
-// Database se connect karana
-function connectToDb() {
+function connectToDB() {
   mongoose
     .connect(
-      "mongodb+srv://vermaniraj9607_db_user:ctWg41Xwxqo4XUCm@cluster0.e5a70yo.mongodb.net/Notes-API",
+      "mongodb+srv://vermaniraj9607_db_user:ctWg41Xwxqo4XUCm@cluster0.e5a70yo.mongodb.net/",
     )
     .then(() => {
-      console.log("Connected to DB");
+      console.log("Database Connected");
     });
 }
 
-connectToDb();
+connectToDB();
+
 app.listen(3000, () => {
-  console.log("Server is running on Port 3000");
+  console.log("Server is running on port 3000");
 });
--- Backend/Day 6/src/app.js
+++ Backend/Day 6/src/app.js
@@ -1,10 +1,36 @@
 const express = require("express");
-
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+let notes = [];
+
+app.post("/notes", (req, res) => {
+  notes.push(req.body);
+  res.status(201).json({
+    message: "Notes Created",
+  });
+});
+
+app.get("/notes", (req, res) => {
   res.status(200).json({
-    message: "Hello World",
+    message: notes,
+  });
+});
+
+app.delete("/notes/:index", (req, res) => {
+  delete notes[req.params.index];
+
+  res.status(200).json({
+    message: "Notes Deleted",
+  });
+});
+
+app.patch("/notes/:index", (req, res) => {
+  notes[req.params.index].description = req.body.description;
+
+  res.status(200).json({
+    message: "Notes Updated",
   });
 });
 
