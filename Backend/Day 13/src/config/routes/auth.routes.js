97e8f2b6ac7acae9b3bd3db429cded5d399d6877
niraj-verma07 <vermaniraj9607@gmail.com>
2026-02-07 23:03:25 +0530

    Day 12 Completed

--- Backend/Day 13/src/config/routes/auth.routes.js
+++ Backend/Day 13/src/config/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const authRouter = express.Router();
 
@@ -18,10 +19,12 @@ authRouter.post("/register", async (req, res) => {
     });
   }
 
+  const hash = crypto.createHash("md5").update(password).digest("hex");
+
   const user = await userModel.create({
     name,
     email,
-    password,
+    password: hash,
   });
 
   const token = jwt.sign(
@@ -41,12 +44,49 @@ authRouter.post("/register", async (req, res) => {
   });
 });
 
-// /api/auth/register
-authRouter.post("/protected", async (req, res) => {
-  console.log(req.cookies);
+// /api/auth/protected
+authRouter.post("/protected", (req, res) => {
+  console.log(req.cookies); //for accessing cookies
 
   res.status(200).json({
     message: "This is protected route",
+  });
+});
+
+// /api/auth/login
+authRouter.post("/login", async (req, res) => {
+  // console.log(req.body);
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found with this email address",
+    });
+  }
+
+  const isPasswordMatch =
+    user.password === crypto.createHash("md5").update(password).digest("hex");
+
+  if (!isPasswordMatch) {
+    return res.status(401).json({
+      message: "Invalid password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
+  res.status(200).json({
+    message: "User logged in",
+    user,
   });
 });
 
