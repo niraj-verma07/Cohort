21241d0a1fa4659c2f6a95e602db129228d11b0a
niraj-verma07 <vermaniraj9607@gmail.com>
2026-03-18 20:52:20 +0530

    Perplexity Day 4

--- Backend/Day 38/Backend/src/controllers/chat.controller.js
+++ Backend/Day 38/Backend/src/controllers/chat.controller.js
@@ -5,31 +5,91 @@ import messageModel from "../models/message.model.js";
 export async function sendMessageController(req, res) {
   const { message, chat: chatId } = req.body;
 
-  const title = await genrateChatTitle(message);
+  let title = null;
+  let chat = null;
 
-  const result = await genrateResponse(message);
+  if (!chatId) {
+    title = await genrateChatTitle(message);
 
-  const chat = await chatModel.create({
-    user: req.user.id,
-    title,
-  });
+    chat = await chatModel.create({
+      user: req.user.id,
+      title,
+    });
+  }
 
   const userMessage = await messageModel.create({
-    chat: chat._id,
+    chat: chatId || chat._id,
     content: message,
     role: "user",
   });
 
+  const messages = await messageModel.find({ chat: chatId });
+
+  const result = await genrateResponse(messages);
+
   const aiMessage = await messageModel.create({
-    chat: chat._id,
+    chat: chatId || chat._id,
     content: result,
     role: "ai",
   });
 
   res.status(201).json({
     title: title,
-    aiMessage: result,
     chat,
+    aiMessage: result,
     aiMessage,
+  });
+}
+
+export async function getChatsController(req, res) {
+  const user = req.user;
+  const chats = await chatModel.find({ user: user.id });
+
+  return res.status(200).json({
+    message: "Chats fetched successfully",
+    chats,
+  });
+}
+
+export async function getMessagesController(req, res) {
+  const { chatId } = req.params;
+
+  const chat = await chatModel.findOne({
+    _id: chatId,
+    user: req.user.id,
+  });
+
+  if (!chat) {
+    return res.status(404).json({
+      message: "Chat not found",
+    });
+  }
+
+  const messages = await messageModel.find({ chat: chatId });
+
+  return res.status(200).json({
+    message: "Messages fetched successfully",
+    messages,
+  });
+}
+
+export async function deleteChatController(req, res) {
+  const { chatId } = req.params;
+
+  const chat = await chatModel.findOneAndDelete({
+    _id: chatId,
+    user: req.user.id,
+  });
+
+  await messageModel.deleteMany({ chat: chatId });
+
+  if (!chat) {
+    return res.status(404).json({
+      message: "Chat not found",
+    });
+  }
+
+  return res.status(200).json({
+    message: "Chat deleted successfully",
   });
 }
--- Backend/Day 38/Backend/src/services/ai.service.js
+++ Backend/Day 38/Backend/src/services/ai.service.js
@@ -1,6 +1,6 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage, SystemMessage } from "langchain";
+import { HumanMessage, SystemMessage, AIMessage } from "langchain";
 
 const geminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
@@ -12,8 +12,16 @@ const mistralModel = new ChatMistralAI({
   apiKey: process.env.MISTRAL_API_KEY,
 });
 
-export async function genrateResponse(message) {
-  const response = await geminiModel.invoke([new HumanMessage(message)]);
+export async function genrateResponse(messages) {
+  const response = await geminiModel.invoke(
+    messages.map((msg) => {
+      if (msg.role === "user") {
+        return new HumanMessage(msg.content);
+      } else if (msg.role === "ai") {
+        return new AIMessage(msg.content);
+      }
+    }),
+  );
   return response.text;
 }
 
