e04c26591905150da90fb124a7304fd9e8116cdd
niraj-verma07 <vermaniraj9607@gmail.com>
2026-03-26 12:00:22 +0530

    Rag Day 2

--- Backend/Day 42/main.js
+++ Backend/Day 42/main.js
@@ -1,22 +1,65 @@
 import { PDFParse } from "pdf-parse";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { MistralAIEmbeddings } from "@langchain/mistralai";
+import dotenv from "dotenv";
+import { Pinecone } from "@pinecone-database/pinecone";
+
 import fs from "fs";
+dotenv.config();
 
-let dataBuffer = fs.readFileSync("./story.pdf");
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY });
+const index = pc.index("rag-cohort");
 
-const parser = new PDFParse({
-  data: dataBuffer,
+// let dataBuffer = fs.readFileSync("./story.pdf");
+
+// const parser = new PDFParse({
+//   data: dataBuffer,
+// });
+
+// const data = await parser.getText();
+
+const embeddings = new MistralAIEmbeddings({
+  apiKey: process.env.MISTRAL_API_KEY,
+  model: "mistral-embed",
 });
 
-const data = await parser.getText();
+// const spiltter = new RecursiveCharacterTextSplitter({
+//   chunkSize: 500,
+//   chunkOverlap: 0,
+// });
 
-console.log(data);
+// const chunks = await spiltter.splitText(data.text);
 
-const spiltter = new RecursiveCharacterTextSplitter({
-  chunkSize: 100,
-  chunkOverlap: 0,
+// const docs = await Promise.all(
+//   chunks.map(async (chunk) => {
+//     const embedding = await embeddings.embedQuery(chunk);
+//     return {
+//       text: chunk,
+//       embedding,
+//     };
+//   }),
+// );
+
+// const result = await index.upsert({
+//   records: docs.map((doc, i) => ({
+//     id: `doc-${i}`,
+//     values: doc.embedding,
+//     metadata: {
+//       text: doc.text,
+//     },
+//   })),
+// });
+
+const queryEmbedding = await embeddings.embedQuery(
+  "How was the internship experience",
+);
+
+console.log(queryEmbedding);
+
+const result = await index.query({
+  vector: queryEmbedding,
+  topK: 2,
+  includeMetadata: true,
 });
 
-const chunks = await spiltter.splitText(data.text);
-
-console.log(chunks);
+console.log(JSON.stringify(result));
