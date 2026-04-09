d53188cff0cc6ac8ddabcf377d3776a5afc45ef5
niraj-verma07 <vermaniraj9607@gmail.com>
2026-04-09 10:29:08 +0530

    feat: initialize backend with Express, MongoDB connection, and basic server setup
    - Added package.json with necessary dependencies and scripts
    - Created server.js to start the Express server and connect to MongoDB
    - Implemented app.js for Express middleware and a basic route
    - Configured environment variables in config.js and established MongoDB connection in db.js

---
+++ Backend/Day 50/Backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is running",
+  });
+});
+
+export default app;
