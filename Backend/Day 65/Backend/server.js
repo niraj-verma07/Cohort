e0a40614af64b945e208171b3c255285cc237611
niraj-verma07 <vermaniraj9607@gmail.com>
2026-05-03 15:27:32 +0530

    Day 157 | Docker

--- Backend/Day 65/Backend/server.js
+++ Backend/Day 65/Backend/server.js
@@ -19,6 +19,13 @@ app.get("/api/data", (req, res) => {
   res.status(200).json(data);
 });
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Server is running smoothly.",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
