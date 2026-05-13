ee1da3b161844df9493ba0d4fa3ac23db15d54d1
niraj-verma07 <vermaniraj9607@gmail.com>
2026-05-13 23:35:20 +0530

    Capstone Day 2

--- Backend/Day 77/sandbox/router/src/app.js
+++ Backend/Day 77/sandbox/router/src/app.js
@@ -13,17 +13,26 @@ app.get("/api/status/readyz", (req, res) => {
   res.status(200).json({ status: "ready" });
 });
 
+const proxies = {};
+
+function getProxy(sandboxId) {
+  const target = `http://sandbox-service-${sandboxId}`;
+
+  if (!proxies[sandboxId]) {
+    proxies[sandboxId] = createProxyMiddleware({
+      target,
+      changeOrigin: true,
+      ws: true,
+    });
+  }
+  return proxies[sandboxId];
+}
+
 app.use((req, res, next) => {
   const host = req.headers.host;
   const sandboxId = host.split(".")[0]; // Extract sandbox ID from subdomain
 
-  const target = `http://sandbox-service-${sandboxId}`;
-
-  return createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    ws: true,
-  })(req, res, next);
+  return getProxy(sandboxId)(req, res, next);
 });
 
 export default app;
