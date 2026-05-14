4e99a176af3f29fce1d5b5bf9ece484adabaa220
niraj-verma07 <vermaniraj9607@gmail.com>
2026-05-14 23:10:45 +0530

    feat: enhance Kubernetes configuration with agent support and volume management

--- Backend/Day 78/sandbox/server/src/kubernetes/pod.js
+++ Backend/Day 78/sandbox/server/src/kubernetes/pod.js
@@ -10,6 +10,27 @@ export async function createPod(sandboxId) {
       },
     },
     spec: {
+      volumes: [
+        {
+          name: "workspace-volume",
+          emptyDir: {},
+        },
+      ],
+      initContainers: [
+        {
+          name: "init-container",
+          image: "template",
+          imagePullPolicy: "IfNotPresent",
+          command: ["sh", "-c", "cp -r /workspace/. /seed/"],
+          volumeMounts: [
+            {
+              name: "workspace-volume",
+              mountPath: "/seed",
+            },
+          ],
+        },
+      ],
+
       containers: [
         {
           image: "template",
@@ -31,6 +52,40 @@ export async function createPod(sandboxId) {
               memory: "500Mi",
             },
           },
+          volumeMounts: [
+            {
+              name: "workspace-volume",
+              mountPath: "/workspace",
+            },
+          ],
+        },
+        {
+          image: "agent",
+          imagePullPolicy: "IfNotPresent",
+          name: "agent-container",
+          ports: [
+            {
+              containerPort: 3000,
+              name: "agent",
+            },
+          ],
+
+          resources: {
+            limits: {
+              cpu: "500m",
+              memory: "1Gi",
+            },
+            requests: {
+              cpu: "250m",
+              memory: "500Mi",
+            },
+          },
+          volumeMounts: [
+            {
+              name: "workspace-volume",
+              mountPath: "/workspace",
+            },
+          ],
         },
       ],
     },
